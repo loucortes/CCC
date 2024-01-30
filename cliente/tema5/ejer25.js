f45cd4bbd1958c52febf4b6c9c2f8b34e5078931
loucortes <lucianohcortes@gmail.com>
2024-01-30 10:07:11 +0100

    Added ex25

---
+++ cliente/tema5/ejer25.js
@@ -0,0 +1,10 @@
+function validateDate(date) {
+  console.log(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/.test(date));
+  }
+  
+  
+  
+  
+  
+  let date = '15/08/2022'
+  validateDate(date); 
