c6385c1b27fbba4632f21f1d42dee52cab0f2b5d
forjoa <joaquin_trujillo@icloud.com>
2024-02-07 12:24:19 +0100

    4th ex

--- joaquin-el-crack/acceso_de_datos/3/3.sql
+++ joaquin-el-crack/acceso_de_datos/3/3.sql
@@ -9,4 +9,13 @@ create table if not exists alumnos (
     lenguaje varchar (255)
 );
 
+create table if not exists cursos (
+    id int primary key auto_increment not null,
+    nombre varchar (255),
+    curso varchar (255),
+    alumno_id int,
+    foreign key (alumno_id) references alumnos(id)
+);
+
 select * from alumnos;
+select * from cursos;
