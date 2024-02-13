fe03ba3a98b01c4f21e1667e863b61fe14d236d2
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-13 13:48:34 +0100

    Add ex4

--- joaquin-el-crack/tema-6/codigo.js
+++ joaquin-el-crack/tema-6/codigo.js
@@ -1,24 +1,24 @@
-
+// Ejercicio 3
 function mostrarEnlaces() {
-    // Obtener el primer enlace y sus atributos
-    let enlaceURL = document
+  // Obtener el primer enlace y sus atributos
+  let enlaceURL = document
     .getElementById('p3')
     .getElementsByTagName('a')[0].href
-    let enlaceTarget = document
+  let enlaceTarget = document
     .getElementById('p3')
     .getElementsByTagName('a')[0].target
-    
-    // bbtener la clase del botón Nodos
-    let claseBoton = document.getElementById('boton').className
-    
-    // Obtener la alineación del primer párrafo
-    let alineacionParrafo = document.getElementById('p1').style.textAlign
-    
-    // Obtener el atributo src de la imagen
-    let srcImagen = document.getElementById('img1').src
-    
-    // Mostrar los resultados en el div con id "resultado"
-    document.getElementById('resultado').innerHTML = `
+
+  // bbtener la clase del botón Nodos
+  let claseBoton = document.getElementById('boton').className
+
+  // Obtener la alineación del primer párrafo
+  let alineacionParrafo = document.getElementById('p1').style.textAlign
+
+  // Obtener el atributo src de la imagen
+  let srcImagen = document.getElementById('img1').src
+
+  // Mostrar los resultados en el div con id "resultado"
+  document.getElementById('resultado').innerHTML = `
     <p>URL del primer enlace: ${enlaceURL}</p>
     <p>Target del primer enlace: ${enlaceTarget}</p>
     <p>Clase del botón Nodos: ${claseBoton}</p>
@@ -28,3 +28,14 @@ function mostrarEnlaces() {
 }
 
 document.getElementById('boton').addEventListener('click', mostrarEnlaces)
+
+// Ejercicio 4
+function add() {
+  const i = document.getElementById('info')
+  const p = document.createElement('p')
+  p.textContent =
+    'Los ciclos formativos de Formación Profesional Básica forman parte de las enseñanzas de Formación Profesional del sistema educativo y responden al perfil profesional 1 definido por el INCUAL. Están cofinanciados por el Programa Operativo del Fondo Social Europeo 2014-2020 al 50%'
+  i.appendChild(p)
+}
+
+document.getElementById('boton').addEventListener('click', add)
